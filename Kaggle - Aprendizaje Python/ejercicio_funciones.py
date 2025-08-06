8f469adf8521b43c0bca72716fb196007459ab33
javier-delatorre <xavier.dlt50@gmail.com>
2025-08-05 23:37:49 -0500

    Mitad de codigo pendiente

---
+++ Kaggle - Aprendizaje Python/ejercicio_funciones.py
@@ -0,0 +1,6 @@
+
+# Utilizar una funcion que calcule los metros cuadrados de una habitacion y se creara otra funcion que calcule la suma de los metros cuadrados de toda una casa segun la cantidad de los cuartos, la segunda funcion sera para sumar los metros cuadrados de 5 habitaciones
+def calculo_metros_cuadrados(metros_lineales_1, metros_lineales_2):
+    metros_cuadrados = metros_lineales_1 * metros_lineales_2
+    return(metros_cuadrados)
+
